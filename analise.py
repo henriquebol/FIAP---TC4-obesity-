6550256618f08368c6acf89e478da849c7c72198
Alysson Tenorio Rego <alyssontrbnb@gmail.com>
2025-11-29 15:49:27 -0300

    atualização e registro de análise nos gráfico boxplot

--- analise.py
+++ analise.py
@@ -30,37 +30,83 @@ def run_analise_page():
                                 ''')
         except:
             st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
-
+##########################################################################################################################
     with tab2:
-        st.markdown("#### 🔹 Análise dos Boxplots das variáveis")
-        try:
-            col1, col2 = st.columns(2, gap="large")
-            with col1:
-                numerical_features = ['Idade', 'Altura(m)', 'Peso(kg)', 'FCVC']
-                for feature in numerical_features:
-                    img_comp = Image.open(f"graphs/box_plot_{feature}.png")
-                    st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"
-            with col2:
-                numerical_features = ['NCP', 'CH2O', 'FAF', 'TUE']
-                for feature in numerical_features:
-                    img_comp = Image.open(f"graphs/box_plot_{feature}.png")
-                    st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"
-
-            with st.expander("Análise", expanded=True):
-                st.markdown('''
-                    ### Análise da Relação entre Variáveis e Obesidade
-
-                    Com base na análise do mapa de calor de correlação e das visualizações (box plots e count plots), podemos extrair os seguintes insights e conclusões sobre os fatores que influenciam o grau de obesidade:
-
-                    *   **Correlações Principais:**
-                        *   **FAVC** (Consumo frequente de alimentos hipercalóricos) apresentou a maior correlação absoluta negativa com **Obesidade**, indicando que pessoas que não consomem frequentemente alimentos hipercalóricos tendem a ter menor obesidade ou peso normal. No entanto, o **LabelEncoder** pode ter invertido o sentido original da correlação, então é importante revisar a interpretação dos valores codificados. Se 'não' foi codificado como 0 e 'sim' como 1, então uma correlação negativa significaria que 'não' (0) está associado a níveis mais altos de obesidade, o que seria contraintuitivo. Reavaliar a codificação é crucial aqui.
-                        *   **Idade** mostrou uma correlação positiva, sugerindo que, em geral, indivíduos mais velhos tendem a apresentar maiores níveis de obesidade.
-                        *   **NCP** (Número de refeições principais) e **FCVC** (Frequência de consumo de vegetais) apresentaram correlações negativas. Para **NCP**, menos refeições principais podem estar associadas a níveis mais altos de obesidade. Para **FCVC**, um menor consumo de vegetais está associado a maiores níveis de obesidade.
-                        *   **CALC** (Consumo de álcool) também mostrou uma correlação negativa, o que, dependendo da codificação, pode indicar que um maior consumo de álcool está associado a menores níveis de obesidade, ou vice-versa. A codificação 'nao', 'As_vezes', 'Frequentemente', 'Sempre' (0,1,2,3) foi feita em ordem crescente, então uma correlação negativa sugere que quanto maior o consumo de álcool, menor o nível de obesidade, o que também pode ser um ponto para reflexão sobre a complexidade da relação.
-                                ''')
+        st.markdown("#### 🔹 Análise dos Boxplots")
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_Peso(kg).png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
         except:
             st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
-    
+        
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_Idade.png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
+        except:
+            st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
+        
+        with st.expander("Análise", expanded=True):
+                st.markdown('''
+                **Peso(kg) e Idade:** Claramente, o **Peso(kg)** aumenta progressivamente com o nível de obesidade. 
+            A **Idade** também demonstra uma tendência de aumento da mediana à medida que o nível de obesidade se agrava, 
+                            especialmente em 'Obesidade_tipo_II' e 'Obesidade_tipo_III'.
+                        ''')
+        #************************************************************************************************#
+        st.markdown('---')
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_Altura(m).png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
+        except:
+            st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
+        
+        with st.expander("Análise", expanded=True):
+                st.markdown('''
+                **Altura(m):** Não parece haver uma relação linear forte ou um padrão claro entre
+                             **Altura(m)** e os níveis de obesidade.
+                        ''')
+        #************************************************************************************************#
+        st.markdown('---')
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_FCVC.png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
+        except:
+            st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
+        
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_NCP.png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
+        except:
+            st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
+        
+        with st.expander("Análise", expanded=True):
+                st.markdown('''
+                        **FCVC (Frequência de consumo de vegetais) e NCP (Número de refeições principais):** As medianas de **FCVC** e **NCP** tendem a diminuir ou 
+                            permanecer estáveis em níveis mais altos de obesidade, o que reforça 
+                            a ideia de que menor consumo de vegetais e menos refeições principais 
+                            podem estar associados à obesidade.
+                            ''')
+        #************************************************************************************************#
+        st.markdown('---')
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_FAF.png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
+        except:
+            st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
+        
+        try:   
+            img_comp = Image.open(f"graphs/box_plot_TUE.png")
+            st.image(img_comp, use_container_width=True) #caption="Comparação de Acurácia entre os Modelos"    
+        except:
+            st.warning("Imagem de comparação não encontrada. Execute o script de treinamento novamente.")
+        
+        with st.expander("Análise", expanded=True):
+                st.markdown('''
+                        **FAF (Frequência de atividade física) e TUE (Tempo de uso de dispositivos tecnológicos):** 
+                        **FAF** tende a diminuir e **TUE** tende a aumentar com o agravamento da obesidade, 
+                            o que é um insight esperado e reforça a importância da atividade física e a limitação 
+                            do tempo de tela para auxiliar na redução dos níveis de obesidade.
+                            ''')         
+ ################################################################################################################   
     with tab3:
         st.markdown("#### 🔹 Análise dos gráficos de Distribuição")
         try:
